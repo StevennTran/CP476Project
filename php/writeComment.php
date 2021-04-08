0827361ecb231513f4275503681cb80b44adeb6d
Quinn Brimley <qbrimley@rogers.com>
2021-04-07 23:53:56 -0400

    writeComment

---
+++ php/writeComment.php
@@ -0,0 +1,47 @@
+<?php
+
+    header("Content-Type: text/plain");
+    
+    //database information
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "book";
+
+    $conn = new mysqli($servername, $username, $password);
+    $sql = "CREATE DATABASE $dbname";
+    $conn->query($sql);
+    $conn->close();
+
+    // create a table
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $sql = "CREATE TABLE forum (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        username CHAR(32) NOT NULL,
+        bookID CHAR(32) NOT NULL,
+        booktitle CHAR(32) NOT NULL,
+        comment CHAR(128) NOT NULL,
+        who CHAR(32) NOT NULL)";
+    $conn->query($sql);
+    $conn->close();
+
+    //user ID
+     $suserName = $_GET["username"];
+     $suserName = "'".$suserName."'";
+    $bookID = $_GET["bookID"];
+    $bookID = "'".$bookID."'";
+     $title = $_GET["title"];
+     $title = "'".$title."'";
+     $comment = $_GET["comment"]
+     $comment = "'".$comment."'";
+    //create the SQL query string
+    $sql = "INSERT INTO forum (username,bookID,booktitle,comment,who) VALUES (".$suserName.",".$bookID.",".$title.",".$comment.", null)";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+    if ($conn->query($sql) === TRUE) {
+        echo "\nInsert successfully";
+      } else {
+        echo "FALSE";
+      }
+    $conn->close();
+?>

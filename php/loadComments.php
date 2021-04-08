5401406aa1c9aeff5bf182869348920ae6e3e0cf
StevennTran <37421431+StevennTran@users.noreply.github.com>
2021-04-07 23:29:56 -0400

    temp

--- php/loadComments.php
+++ php/loadComments.php
@@ -26,16 +26,16 @@
     $conn->close();
 
     //user ID
-    $suserName = $_GET["username"];
+    // $suserName = $_GET["username"];
     $bookID = $_GET["bookID"];
-    $title = $_GET["title"];
+    // $title = $_GET["title"];
     //create the SQL query string
     $sql = "Select * from forum where bookID ='".$bookID."'";
               
     $info = "";
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
-    $result = $conn->query($sql)->fetch_all;
+    $result = $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
     if ($result->num_rows > 0) {
         foreach ($result as $row){
             $user = $row['username'];
